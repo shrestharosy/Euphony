878735f123c8580fe209a15924c7852296edd3eb
Rosy <rosy@fusemachines.com>
2018-12-05 19:20:42 +0545

    [INITIAL SETUP] : Setup react project with typescript
    - Add tslint configurations

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
