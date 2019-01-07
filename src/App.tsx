e171fee1ae1fd7d8dd81723a8e25be9af418aea7
Rosy <rosy@fusemachines.com>
2019-01-07 14:53:02 +0545

    [STYLE] : Add scss styles and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,76 @@
 /* global chrome */
 // gives access to chrome browser methods
 import * as React from "react";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Ola Euph
-          </p>
-        </header>
+      <div>
+        <Nav tabs={true}>
+          <NavItem>
+            <NavLink
+            // className={classnames({ active: this.state.activeTab === '1' })}
+            // onClick={() => { this.toggle('1'); }}
+            >
+              Tab1
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+            // className={classnames({ active: this.state.activeTab === '2' })}
+            // onClick={() => { this.toggle('2'); }}
+            >
+              Moar Tabs
+            </NavLink>
+          </NavItem>
+        </Nav>
+        {/* <TabContent activeTab={this.state.activeTab}> */}
+        <TabContent>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 1 Contents</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="6">
+                <Card body={true}>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+              <Col sm="6">
+                <Card body={true}>
+                  <CardTitle>Special Title Treatment</CardTitle>
+                  <CardText>
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </CardText>
+                  <Button>Go somewhere</Button>
+                </Card>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles/scss/paper-dashboard.scss";
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
